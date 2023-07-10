68619b5630b1daa28601850edcd28dddd83bde50
Semih Gervan <semihgervan@hotmail.com>
2023-07-10 20:58:24 +0300

    S10-challenge

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
+import { toast } from 'react-toastify';
+
 export const NOT_EKLE = "NOT_EKLE"
 export const NOT_SIL = "NOT_SIL"
 
 export function notEkle(not) {
+  return { type: NOT_EKLE, payload: not }
   // ...
 }
 
 export function notSil(notId) {
+  return { type: NOT_SIL, payload: notId }
   // ...
 }
 
@@ -16,6 +20,8 @@ export const notEkleAPI = (yeniNot) => dispatch => {
     .post("https://httpbin.org/anything", yeniNot)
     .then((res) => {
       if (res.status === 200) {
+        dispatch(notEkle(res.data.json))
+        toast.success("Başarıyla Eklendi!")
         // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notEkle ile dispatch edin
       }
     })
@@ -28,6 +34,8 @@ export const notSilAPI = (id) => dispatch => {
     .delete("https://httpbin.org/anything", { data: id })
     .then((res) => {
       if (res.status === 200) {
+        dispatch(notSil(res.data.data))
+        toast.success("Başarıyla Silindi!")
         // res.data objesi içerisinden ihtiyaç duyduğunuz değeri bulun ve oluşturduğunuz notSil ile dispatch edin 
       }
     })
--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { legacy_createStore as createStore } from 'redux';
+import reducers from "./reducers"
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const store = createStore(reducers, applyMiddleware(thunk))
 
 root.render(
-  <BrowserRouter>
-    <>
-      <App />
-    </>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <>
+        <ToastContainer />
+        <App />
+      </>
+    </BrowserRouter>
+  </Provider>
 );
